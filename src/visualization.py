a7c513997aef459c0cbeb11d9ba696556c7ce7cf
andika <dika.kusuma23@gmail.com>
2019-03-16 15:31:29 +0700

    Added base chart for indonesia's progress'

--- src/visualization.py
+++ src/visualization.py
@@ -1,25 +1,55 @@
+import json
 import pygal
-from pygal.style import Style
+
+
+class IndonesiaProgressChart:
+
+
+    def __init__(self, data):
+        self.chart = pygal.StackedLine(fill=True)
+        self.output = 'asset/indonesia-progress.png'
+        self.title = 'Indonesia progress medal'
+        self.data = data
+
+
+    def draw(self):
+        self.chart.title = self.title
+
+        # draw using the json data
+        years, golds, silvers, bronzes, ranks = [], [], [], [], []
+        for yearly_medal in self.data:
+            years.append(int(yearly_medal['Tahun']))
+            medals = yearly_medal['Data']
+            golds.append(int(medals['Emas']))
+            silvers.append(int(medals['Perak']))
+            bronzes.append(int(medals['Perunggu']))
+            ranks.append(int(medals['Peringkat']))
+
+        self.chart.x_labels = years
+        self.chart.add('Emas', golds)
+        self.chart.add('Perak', silvers)
+        self.chart.add('Perunggu', bronzes)
+
+
+    def render_to_png(self):
+        self.chart.render_to_png(self.output)
 
 
 if __name__ == '__main__':
-    custom_style = Style(
-        background='transparent',
-        plot_background='transparent',
-        foreground='#53E89B',
-        foreground_strong='#53A0E8',
-        foreground_subtle='#630C0D',
-        opacity='.6',
-        opacity_hover='.9',
-        transition='400ms ease-in',
-        colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'))
-
-    line_chart = pygal.HorizontalStackedBar(style=custom_style)
-    line_chart.title = 'Browser usage evolution (in %)'
-    line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-    line_chart.render_to_png('temp.png')
+    # indonesia progress chart
+    with open('data/indonesia_progress.json') as json_file:
+        data = json.load(json_file)
+    indonesia_progress_chart = IndonesiaProgressChart(data)
+    indonesia_progress_chart.draw()
+    indonesia_progress_chart.render_to_png()
+#    custom_style = Style(
+#        background='transparent',
+#        plot_background='transparent',
+#        foreground='#53E89B',
+#        foreground_strong='#53A0E8',
+#        foreground_subtle='#630C0D',
+#        opacity='.6',
+#        opacity_hover='.9',
+#        transition='400ms ease-in',
+#        colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'))
 
