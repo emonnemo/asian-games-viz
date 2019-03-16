052d77d5350ba68bbc84a14415fd937a1b12a349
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2019-03-16 23:21:30 +0700

    Added viz for indonesia sports medal on 2018

--- src/visualization.py
+++ src/visualization.py
@@ -76,6 +76,56 @@ class IndonesiaProgressChart(BaseChart):
     def render_to_png(self):
         self.chart.render_to_png(self.output)
 
+class IndonesiaSports2018Chart(BaseChart):
+
+    def __init__(self, **kwargs):
+        self.output = 'asset/sports-indonesia.png'
+        self.title = 'Perolahan Medali Indonesia Per Cabang 2018'
+        super().__init__(**kwargs)
+
+
+    def filter(self, filter_config):
+        '''
+        Filter by changing the data in shown_data
+        according to filter_config
+        '''
+        pass
+
+    
+    def draw(self):
+
+        self.chart = pygal.HorizontalStackedBar(style=self.style, fill=True)
+        self.chart.title = self.title
+
+        # draw using the json data
+        sport_names = []
+        golds, silvers, bronzes = [], [], []
+        rest = []
+        for sports in self.shown_data:
+            if not sports['Cabor'] == "Atletik":
+                sport_names.append(sports['Cabor'])
+                medals = sports['Medali']
+
+                gold = int(medals['Emas'])
+                silver = int(medals['Perak'])
+                bronze = int(medals['Perunggu'])
+                rest_medal = int(medals["Total Seluruh Medali"]) - (gold + silver + bronze)
+
+                golds.append(gold)
+                silvers.append(silver)
+                bronzes.append(bronze)
+                rest.append(rest_medal)
+
+        self.chart.x_labels = sport_names
+        self.chart.add('Perunggu', bronzes)
+        self.chart.add('Perak', silvers)
+        self.chart.add('Emas', golds)
+        self.chart.add('Medali Tersisa', rest)
+
+
+    def render_to_png(self):
+        self.chart.render_to_png(self.output)
+
 
 if __name__ == '__main__':
     # basic style
@@ -91,10 +141,30 @@ if __name__ == '__main__':
         colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'),
     )
 
+    sports_style = Style(
+        background='white',
+        plot_background='transparent',
+        foreground='#000000',
+        foreground_strong='#53A0E8',
+        foreground_subtle='#630C0D',
+        opacity='.6',
+        opacity_hover='.9',
+        transition='400ms ease-in',
+        colors=('#cd7f32', '#e5e4e2', '#ffff00', '#000000', '#E89B53'),
+    )
+
     # indonesia progress chart
     with open('data/indonesia_progress.json') as json_file:
         data = json.load(json_file)
+
+    with open('data/indonesia_2018.json') as json_file:
+        sports_2018_data = json.load(json_file)
+
     indonesia_progress_chart = IndonesiaProgressChart(data=data, style=custom_style)
     indonesia_progress_chart.draw()
     indonesia_progress_chart.render_to_png()
+
+    indonesia_2018_sports_chart = IndonesiaSports2018Chart(data=sports_2018_data, style=sports_style)
+    indonesia_2018_sports_chart.draw()
+    indonesia_2018_sports_chart.render_to_png()
 
