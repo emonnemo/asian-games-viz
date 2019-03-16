885c28e1c3db321b745e8ebb6be7d9105fddaf13
andika <dika.kusuma23@gmail.com>
2019-03-16 21:12:13 +0700

    Added base chart class and transparent background style for charts

--- src/visualization.py
+++ src/visualization.py
@@ -1,23 +1,65 @@
+from abc import ABC
+from abc import abstractmethod
+from pygal.style import Style
+
 import json
 import pygal
 
 
-class IndonesiaProgressChart:
+class BaseChart(ABC):
+    '''
+    Base abstract class for charts
+    '''
+
+    def __init__(self, data, style=None):
+        self.data = data
+        self.shown_data = data
+        self.style = style
 
 
-    def __init__(self, data):
-        self.chart = pygal.StackedLine(fill=True)
+    @abstractmethod
+    def filter(self, filter_config):
+        pass
+
+
+    def reset(self):
+        self.shown_data = data
+
+
+    @abstractmethod
+    def draw(self):
+        return NotImplementedError
+
+    
+    @abstractmethod
+    def render_to_png(self):
+        return NotImplementedError
+
+
+class IndonesiaProgressChart(BaseChart):
+
+
+    def __init__(self, **kwargs):
         self.output = 'asset/indonesia-progress.png'
         self.title = 'Indonesia progress medal'
-        self.data = data
+        super().__init__(**kwargs)
 
 
+    def filter(self, filter_config):
+        '''
+        Filter by changing the data in shown_data
+        according to filter_config
+        '''
+        pass
+
+    
     def draw(self):
+        self.chart = pygal.StackedLine(fill=True, style=self.style)
         self.chart.title = self.title
 
         # draw using the json data
         years, golds, silvers, bronzes, ranks = [], [], [], [], []
-        for yearly_medal in self.data:
+        for yearly_medal in self.shown_data:
             years.append(int(yearly_medal['Tahun']))
             medals = yearly_medal['Data']
             golds.append(int(medals['Emas']))
@@ -36,20 +78,23 @@ class IndonesiaProgressChart:
 
 
 if __name__ == '__main__':
+    # basic style
+    custom_style = Style(
+        background='transparent',
+        plot_background='transparent',
+        foreground='#53E89B',
+        foreground_strong='#53A0E8',
+        foreground_subtle='#630C0D',
+        opacity='.6',
+        opacity_hover='.9',
+        transition='400ms ease-in',
+        colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'),
+    )
+
     # indonesia progress chart
     with open('data/indonesia_progress.json') as json_file:
         data = json.load(json_file)
-    indonesia_progress_chart = IndonesiaProgressChart(data)
+    indonesia_progress_chart = IndonesiaProgressChart(data=data, style=custom_style)
     indonesia_progress_chart.draw()
     indonesia_progress_chart.render_to_png()
-#    custom_style = Style(
-#        background='transparent',
-#        plot_background='transparent',
-#        foreground='#53E89B',
-#        foreground_strong='#53A0E8',
-#        foreground_subtle='#630C0D',
-#        opacity='.6',
-#        opacity_hover='.9',
-#        transition='400ms ease-in',
-#        colors=('#E853A0', '#E8537A', '#E95355', '#E87653', '#E89B53'))
 
